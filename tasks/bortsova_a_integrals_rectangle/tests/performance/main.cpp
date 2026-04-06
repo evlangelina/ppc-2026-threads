7d12501da8f2bafec0d45f1c10f2bf4cc9a0e49c
evlangelina <evl.bortsova@gmail.com>
2026-04-06 20:18:16 +0300

    add stl build

--- tasks/bortsova_a_integrals_rectangle/tests/performance/main.cpp
+++ tasks/bortsova_a_integrals_rectangle/tests/performance/main.cpp
@@ -6,6 +6,7 @@
 #include "bortsova_a_integrals_rectangle/common/include/common.hpp"
 #include "bortsova_a_integrals_rectangle/omp/include/ops_omp.hpp"
 #include "bortsova_a_integrals_rectangle/seq/include/ops_seq.hpp"
+#include "bortsova_a_integrals_rectangle/stl/include/ops_stl.hpp"
 #include "bortsova_a_integrals_rectangle/tbb/include/ops_tbb.hpp"
 #include "util/include/perf_test_util.hpp"
 
@@ -40,7 +41,8 @@ namespace {
 
 const auto kAllPerfTasks =
     ppc::util::MakeAllPerfTasks<InType, BortsovaAIntegralsRectangleOMP, BortsovaAIntegralsRectangleSEQ,
-                                BortsovaAIntegralsRectangleTBB>(PPC_SETTINGS_bortsova_a_integrals_rectangle);
+                                BortsovaAIntegralsRectangleSTL, BortsovaAIntegralsRectangleTBB>(
+        PPC_SETTINGS_bortsova_a_integrals_rectangle);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
